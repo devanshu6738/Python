e1fefde7554d18932264f857f79f13bb999689fa
Devanshu Gupta <devanshu9167@gmail.com>
2025-01-31 18:56:21 +0530

    #

---
+++ print delete/delete.py
@@ -0,0 +1,17 @@
+a=45
+b="devanshu gupta"
+del(b)
+# print(b)
+print(type(a))
+# typecasting
+d=92
+print(type(str(d)))
+print(type(d))
+e=10.5
+print(int(e))
+
+#variable naming case
+#camel-case - variableName
+# Pascal-case - VariableName
+# Snake case- variable_name
+# Screaming Snake case - VARIABLE_NAME
---
+++ print delete/print.py
@@ -0,0 +1,4 @@
+a=45
+b="Devanshu gupta"
+print(id(a))
+print(b)
