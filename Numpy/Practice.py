0bb85044ff3ac8867d6b408ec1c8fdb72cf7aeef
devanshu6738 <9devanshugupta@gmail.com>
2026-04-08 19:44:04 +0530

    numpy revision

--- Numpy/Practice.py
+++ Numpy/Practice.py
@@ -10,3 +10,20 @@ print(arr2 is arr)
 arr3=np.copy(arr)
 print(arr3 is arr)
 
+print(arr.shape)
+
+print(list(range(5)))
+
+print(np.arange(1,50,2,dtype='int32'))
+
+print(np.linspace(1,100,num=20,endpoint=False))
+print(np.linspace(1,100,num=20,endpoint=True))
+
+
+print(np.linspace(1,100,num=20,endpoint=True,retstep=True))
+
+print(np.logspace(1,50,num=20,endpoint=True))
+
+print(np.zeros((3,4)))
+
+print(5*np.ones((3,4)))
